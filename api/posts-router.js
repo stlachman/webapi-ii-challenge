806645c5f775a5d1fe183b9b33525ee584c4ff00
Seth Lachman <lachmanseth17@gmail.com>
2019-05-28 17:43:45 -0400

    Added delete method.

--- api/posts-router.js
+++ api/posts-router.js
@@ -110,4 +110,29 @@ router.get("/:id/comments", (req, res) => {
     });
 });
 
+// DELETE  /api/posts/:id
+
+router.delete("/:id", (req, res) => {
+  const postId = req.params.id;
+  Posts.findById(postId)
+    .then(post => {
+      if (post && post.length) {
+        Posts.remove(postId)
+          .then(deletedPost => res.status(200).json(post))
+          .catch(err =>
+            res.status(500).json({ error: "The post could not be removed" })
+          );
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err =>
+      res
+        .status(500)
+        .json({ error: "The post information could not be retrieved." })
+    );
+});
+
 module.exports = router;
