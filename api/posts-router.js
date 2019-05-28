a7fd40b53d72173d4d773828d30551c54d09f0b9
Seth Lachman <lachmanseth17@gmail.com>
2019-05-28 16:46:49 -0400

    Addd GET request for comments.

--- api/posts-router.js
+++ api/posts-router.js
@@ -34,7 +34,7 @@ router.get("/", (req, res) => {
     });
 });
 
-// GET Posts /api/posts/:id
+// GET  /api/posts/:id
 router.get("/:id", (req, res) => {
   const postId = req.params.id;
   Posts.findById(postId)
@@ -54,6 +54,25 @@ router.get("/:id", (req, res) => {
     });
 });
 
-// GET
+// GET /api/posts/:id/comments
+
+router.get("/:id/comments", (req, res) => {
+  const postId = req.params.id;
+  Posts.findPostComments(postId)
+    .then(post => {
+      if (post && post.length) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: "The comments information could not be retrieved." });
+    });
+});
 
 module.exports = router;
