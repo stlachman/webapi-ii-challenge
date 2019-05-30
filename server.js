c54a238f7bf063b11a90ad66c1ee702330e0c065
Seth Lachman <lachmanseth17@gmail.com>
2019-05-30 16:57:17 -0400

    Added code for react app.

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const postRoutes = require("./api/posts-router.js");
 server.use(express.json());
 server.use(cors());
 
+server.use(express.static(path.join(__dirname, "client/build")));
+
 server.use("/api/posts", postRoutes);
+
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 module.exports = server;
